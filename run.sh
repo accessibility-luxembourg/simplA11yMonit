48cc516ad8e790b9470fe90d12f216e18a518b09
Alain Vagner <alain.vagner@sip.etat.lu>
2021-07-23 10:29:47 +0200

    cleaned output log

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@ cp -r static/template-grille-audit-simplifie out
 NODE_ICU_DATA=./node_modules/full-icu node ./test/accessTests.js $@
 fileName=$(node test/getSiteName.js $1)
 cd out/template-grille-audit-simplifie
-7z a -tzip ../$fileName.xlsx *
+7z a -tzip  ../$fileName.xlsx * 1> /dev/null
 cd .. && rm -rf template-grille-audit-simplifie
